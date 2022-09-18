78dc00d39af7289497c518a9f13eacedd5ed93cf
Aleksandra <librra_90@yahoo.com>
2022-09-18 10:55:27 +1000

    Добавила решение четвертой задачи

---
+++ 4_Задача/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int num = 2;
+Console.Write($"Все четные числа от 1 до {a}: ");
+while(num <= a)
+{
+    Console.Write(num + ", ");
+    num = num + 2;
+}
+Console.WriteLine();
